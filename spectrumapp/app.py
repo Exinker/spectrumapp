81ffefb02eab8fccb7842f24051e2a56fb6077b3
Exinker <exinker@gmail.com>
2023-06-09 16:40:57 +0700

    refac: updated window module and added setdefault actions and menubar

--- spectrumapp/app.py
+++ spectrumapp/app.py
@@ -6,10 +6,9 @@ from PySide6 import QtCore, QtGui, QtWidgets
 
 from spectrumapp.core.config import setdefault_config
 from spectrumapp.core.logging import setdefault_logging, log
-from spectrumapp.core.setting import setdefault_setting, get_setting, set_setting
-from spectrumapp.core.utils import pave
-from spectrumapp.utils.find import find_window
-from spectrumapp.utils.modifier import wait, attempt, MessageLevel
+from spectrumapp.core.setting import setdefault_setting
+from spectrumapp.utils.modifier import wait, attempt
+from spectrumapp.window.window import BaseMainWindow
 from spectrumapp.window.splashScreenWindow import splashscreen
 
 
@@ -69,207 +68,20 @@ class EmptyCentralWidget(QtWidgets.QWidget):
         pass
 
 
-class BaseMainWindow(QtWidgets.QMainWindow):
+class Window(BaseMainWindow):
 
-    @splashscreen(progress=70, info='<strong>LOADING</strong> user interface...')
-    def __init__(self, *args, **kwargs):
-        super().__init__(objectName='mainWindow', *args, **kwargs)
+    def __init__(self, *args, show: bool = False, **kwargs):
+        super().__init__(*args, show=show, **kwargs)
 
-        # style
-        filepath = pave(os.path.join('.', 'static', 'app.css'))
-        style = open(filepath, 'r').read()
-        self.setStyleSheet(style)
-
-        # icon
-        filepath = pave(os.path.join('.', 'static', 'icon.ico'))
-        icon = QtGui.QIcon(filepath)
-        self.setWindowIcon(icon)
-
-        # title
-        filepath = ''
-        title = f'{APPLICATION_NAME} - [{filepath}]'
-        self.setWindowTitle(title)
+        self._setdefault_actions()
+        self._setdefault_menubar()
 
         # widget
         widget = EmptyCentralWidget(parent=self)
         self.setCentralWidget(widget)
 
-        # menu
-        menuBar = self.menuBar()
-
-        fileMenu = menuBar.addMenu('&File')
-        action = QtGui.QAction('&Open', self)
-        action.setShortcut('Ctrl+O')
-        action.setStatusTip('Open file...')
-        action.triggered.connect(self._onOpenAction)
-        fileMenu.addAction(action)
-        fileMenu.addSeparator()
-        action = QtGui.QAction('&Refresh', self)
-        action.setShortcut(QtGui.QKeySequence('Ctrl+R'))
-        action.setShortcutContext(QtCore.Qt.ApplicationShortcut)
-        action.triggered.connect(self._onRefreshAppAction)
-        fileMenu.addAction(action)
-        action = QtGui.QAction('&Reset', self)
-        action.setShortcut(QtGui.QKeySequence('Ctrl+Shift+R'))
-        action.setShortcutContext(QtCore.Qt.ApplicationShortcut)
-        action.triggered.connect(self._onResetAppAction)
-        fileMenu.addAction(action)
-        fileMenu.addSeparator()
-        action = QtGui.QAction('&Quit', self)
-        action.setShortcut(QtGui.QKeySequence('Ctrl+Q'))
-        action.setStatusTip('Quit')
-        action.triggered.connect(self._onQuitAction)
-        fileMenu.addAction(action)
-
-        instrumentsMenu = menuBar.addMenu('&Instruments')
-        # action = QtGui.QAction('&Filtrate', self)
-        # action.setShortcut(QtGui.QKeySequence('Ctrl+F'))
-        # action.setShortcutContext(QtCore.Qt.ApplicationShortcut)
-        # action.setStatusTip('Filtrate probes by condition...')
-        # action.triggered.connect(self._onOpenFiltrateWindowAction)
-        # instrumentsMenu.addAction(action)
-
-        helpMenu = menuBar.addMenu('&Help')
-        action = QtGui.QAction('&Help', self)
-        action.setShortcutContext(QtCore.Qt.ApplicationShortcut)
-        action.triggered.connect(self._onOpenHelpWindowAction)
-        helpMenu.addAction(action)
-        # action = QtGui.QAction('&Report Issue', self)
-        # action.setShortcutContext(QtCore.Qt.ApplicationShortcut)
-        # action.triggered.connect(self._onOpenReportIssueWindowAction)
-        # helpMenu.addAction(action)
-        action = QtGui.QAction('&About', self)
-        action.triggered.connect(self._onOpenAboutWindowAction)
-        helpMenu.addAction(action)
-
-        # geometry
-        geometry = get_setting(key=f'geometry/{self.objectName()}')
-        if geometry:
-            self.setGeometry(geometry)
-            self.setWindowState(QtCore.Qt.WindowActive)
-        else:
-            self.setWindowState(QtCore.Qt.WindowMaximized | QtCore.Qt.WindowActive)
-
         #
         self.show()
-
-    # --------        slots        --------
-    @wait
-    def _onOpenAction(self):
-        """Open new file action."""
-
-        # settings
-        filedir = QtWidgets.QFileDialog().getExistingDirectory(
-            parent=self,
-            caption='Choose directory with Atom\'s dump file:',
-            dir=os.path.join(os.getcwd(), 'data'),
-        )
-
-        #
-        files = ('dump.pkl', )
-        if any([os.path.isfile(os.path.join(filedir, file)) for file in files]):
-            filedir = os.path.abspath(filedir)
-            head, tail = os.path.split(filedir)
-
-            # update settings
-            set_setting(
-                key='config/filedir',
-                value=filedir,
-            )
-            set_setting(
-                key='config/filename',
-                value=tail,
-            )
-            setdefault_setting()
-
-            # reset app
-            self._onResetAppAction()
-
-    @wait
-    @splashscreen(delay=5)
-    def _onResetAppAction(self, *args, **kwargs):
-        '''An action occurs due to change file.'''
-
-        # reset app
-        app = QtWidgets.QApplication.instance()
-        app.reset()
-
-        # update title
-        filedir = ''
-        title = f"{APPLICATION_NAME} - [{filedir}]"
-        self.setWindowTitle(title)
-
-        # reset windows
-        for window in app.topLevelWidgets():
-            window_name = window.objectName()
-
-            if window_name in ('mainWindow',):
-                pass
-            else:
-                window.close()
-
-    @wait
-    def _onRefreshAppAction(self, *args, **kwargs):
-        """Refresh the widgets at the window and refresh all sub windows."""
-        app = QtWidgets.QApplication.instance()
-
-        for window in app.topLevelWidgets():
-            window_name = window.objectName()
-
-            # update core window
-            if window_name in ('mainWindow',):
-                window.centralWidget().refresh()
-
-            # update instrument windows
-            pass
-
-            # close info windows
-            if window_name in ('filtrationWindow', 'helpWindow', 'reportIssueWindow', 'aboutWindow'):
-                window.close()
-
-    @wait
-    def _onQuitAction(self):
-        self.close()
-
-    @wait
-    @attempt(level=MessageLevel.error)
-    def _onOpenHelpWindowAction(self):
-        window_name = 'helpWindow'
-
-        window = find_window(window_name)
-        if window is not None:
-            window.show()
-
-        else:
-            window = HelpWindow(
-                parent=self,
-                flags=QtCore.Qt.Window | QtCore.Qt.WindowStaysOnTopHint,
-            )
-
-    @wait
-    @attempt(level=MessageLevel.error)
-    def _onOpenAboutWindowAction(self):
-        window_name = 'aboutWindow'
-
-        window = find_window(window_name)
-        if window is not None:
-            window.show()
-
-        else:
-            window = AboutWindow(
-                parent=self,
-                flags=QtCore.Qt.Window | QtCore.Qt.WindowStaysOnTopHint,
-            )
-
-    # --------        events        --------
-    @log(msg='app: close event')
-    def closeEvent(self, event: QtCore.QEvent):
-
-        # geometry
-        set_setting(key=f'geometry/{self.objectName()}', value=self.geometry())
-
-        #
-        return super().closeEvent(event)
 
 
 class Application(QtWidgets.QApplication):
@@ -288,9 +100,7 @@ class Application(QtWidgets.QApplication):
     @splashscreen()
     @log(msg='app: run')
     def run(self):
-
-        # window
-        self.window = BaseMainWindow()
+        self.window = Window()
 
     @wait
     @log(msg='app: refresh')
--- spectrumapp/window/window.py
+++ spectrumapp/window/window.py
@@ -1,10 +1,248 @@
 
+import os
+from collections.abc import Mapping
+
 from PySide6 import QtCore, QtGui, QtWidgets
 
-from spectrumapp.core.setting import get_setting, set_setting
+from spectrumapp.core.config import setdefault_config
+from spectrumapp.core.logging import setdefault_logging, log
+from spectrumapp.core.setting import setdefault_setting, get_setting, set_setting
+from spectrumapp.core.utils import pave
+from spectrumapp.utils.find import find_window
+from spectrumapp.utils.modifier import wait, attempt, MessageLevel
+from spectrumapp.window.splashScreenWindow import splashscreen
 
 
-class Window(QtWidgets.QWidget):
+APPLICATION_NAME = os.environ.get('APPLICATION_NAME', '')
+
+
+class BaseMainWindow(QtWidgets.QMainWindow):
+    _actions: Mapping[str, QtGui.QAction] = dict()
+
+    @splashscreen(progress=70, info='<strong>LOADING</strong> user interface...')
+    def __init__(self, *args, show: bool = False, **kwargs):
+        super().__init__(objectName='mainWindow', *args, **kwargs)
+
+        # style
+        filepath = pave(os.path.join('.', 'static', 'app.css'))
+        style = open(filepath, 'r').read()
+        self.setStyleSheet(style)
+
+        # icon
+        filepath = pave(os.path.join('.', 'static', 'icon.ico'))
+        icon = QtGui.QIcon(filepath)
+        self.setWindowIcon(icon)
+
+        # title
+        filepath = ''
+        title = f'{APPLICATION_NAME} - [{filepath}]'
+        self.setWindowTitle(title)
+
+        # geometry
+        geometry = get_setting(key=f'geometry/{self.objectName()}')
+        if geometry:
+            self.setGeometry(geometry)
+            self.setWindowState(QtCore.Qt.WindowActive)
+        else:
+            self.setWindowState(QtCore.Qt.WindowMaximized | QtCore.Qt.WindowActive)
+
+        #
+        if show:
+            self.show()
+
+    # --------        setup        --------
+    def _register_action(self, action: QtGui.QAction, key: str) -> None:
+
+        # add action to window 
+        self.addAction(action)
+
+        # register action
+        self._actions[key] = action
+
+    def _setdefault_actions(self) -> None:
+
+        action = QtGui.QAction('&Open...', self)
+        action.setShortcut('Ctrl+O')
+        action.setShortcutContext(QtCore.Qt.ShortcutContext.WindowShortcut)
+        action.setToolTip('Open file...')
+        action.triggered.connect(self._onOpenAction)
+        self._register_action(action, key='open')
+
+        action = QtGui.QAction('&Refresh', self)
+        action.setShortcut(QtGui.QKeySequence('Ctrl+R'))
+        action.setShortcutContext(QtCore.Qt.ApplicationShortcut)
+        action.triggered.connect(self._onRefreshAppAction)
+        self._register_action(action, key='refresh')
+
+        action = QtGui.QAction('&Reset', self)
+        action.setShortcut(QtGui.QKeySequence('Ctrl+Shift+R'))
+        action.setShortcutContext(QtCore.Qt.ApplicationShortcut)
+        action.triggered.connect(self._onResetAppAction)
+        self._register_action(action, key='reset')
+
+        action = QtGui.QAction('&Quit', self)
+        action.setShortcut(QtGui.QKeySequence('Ctrl+Q'))
+        action.setStatusTip('Quit')
+        action.triggered.connect(self._onQuitAction)
+        self._register_action(action, key='quit')
+
+        action = QtGui.QAction('&Help', self)
+        action.setShortcutContext(QtCore.Qt.ApplicationShortcut)
+        action.triggered.connect(self._onOpenHelpWindowAction)
+        self._register_action(action, key='help')
+
+        action = QtGui.QAction('&About', self)
+        action.triggered.connect(self._onOpenAboutWindowAction)
+        self._register_action(action, key='about')
+
+    def _setdefault_menubar(self) -> None:
+
+        menubar = QtWidgets.QMenuBar()
+        self.setMenuBar(menubar)
+
+        menu = QtWidgets.QMenu(title='&File', parent=self)
+        menu.addAction(self._actions['open'])
+        menu.addSeparator()
+        menu.addAction(self._actions['refresh'])
+        menu.addAction(self._actions['reset'])
+        menu.addSeparator()
+        menu.addAction(self._actions['quit'])
+        menubar.addMenu(menu)
+
+        menu = QtWidgets.QMenu(title='&Instrument', parent=self)
+        menu.setEnabled(False)
+        menubar.addMenu(menu)
+
+        menu = QtWidgets.QMenu(title='&Settings', parent=self)
+        menu.setEnabled(False)
+        menubar.addMenu(menu)
+
+        menu = QtWidgets.QMenu(title='&Information', parent=self)
+        menu.addAction(self._actions['help'])
+        menu.addSeparator()
+        menu.addAction(self._actions['about'])
+        menubar.addMenu(menu)
+
+    # --------        slots        --------
+    @wait
+    def _onOpenAction(self):
+        """Open new file action."""
+
+        # settings
+        filedir = QtWidgets.QFileDialog().getExistingDirectory(
+            parent=self,
+            caption='Choose directory with Atom\'s dump file:',
+            dir=os.path.join(os.getcwd(), 'data'),
+        )
+
+        #
+        files = ('dump.pkl', )
+        if any([os.path.isfile(os.path.join(filedir, file)) for file in files]):
+            filedir = os.path.abspath(filedir)
+            head, tail = os.path.split(filedir)
+
+            # update settings
+            set_setting(
+                key='config/filedir',
+                value=filedir,
+            )
+            set_setting(
+                key='config/filename',
+                value=tail,
+            )
+            setdefault_setting()
+
+            # reset app
+            self._onResetAppAction()
+
+    @wait
+    @splashscreen(delay=5)
+    def _onResetAppAction(self, *args, **kwargs):
+        '''An action occurs due to change file.'''
+
+        # reset app
+        app = QtWidgets.QApplication.instance()
+        app.reset()
+
+        # update title
+        filedir = ''
+        title = f"{APPLICATION_NAME} - [{filedir}]"
+        self.setWindowTitle(title)
+
+        # reset windows
+        for window in app.topLevelWidgets():
+            window_name = window.objectName()
+
+            if window_name in ('mainWindow',):
+                pass
+            else:
+                window.close()
+
+    @wait
+    def _onRefreshAppAction(self, *args, **kwargs):
+        """Refresh the widgets at the window and refresh all sub windows."""
+        app = QtWidgets.QApplication.instance()
+
+        for window in app.topLevelWidgets():
+            window_name = window.objectName()
+
+            # update core window
+            if window_name in ('mainWindow',):
+                window.centralWidget().refresh()
+
+            # update instrument windows
+            pass
+
+            # close info windows
+            if window_name in ('filtrationWindow', 'helpWindow', 'reportIssueWindow', 'aboutWindow'):
+                window.close()
+
+    @wait
+    def _onQuitAction(self):
+        self.close()
+
+    @wait
+    @attempt(level=MessageLevel.error)
+    def _onOpenHelpWindowAction(self):
+        window_name = 'helpWindow'
+
+        window = find_window(window_name)
+        if window is not None:
+            window.show()
+
+        else:
+            window = HelpWindow(
+                parent=self,
+                flags=QtCore.Qt.Window | QtCore.Qt.WindowStaysOnTopHint,
+            )
+
+    @wait
+    @attempt(level=MessageLevel.error)
+    def _onOpenAboutWindowAction(self):
+        window_name = 'aboutWindow'
+
+        window = find_window(window_name)
+        if window is not None:
+            window.show()
+
+        else:
+            window = AboutWindow(
+                parent=self,
+                flags=QtCore.Qt.Window | QtCore.Qt.WindowStaysOnTopHint,
+            )
+
+    # --------        events        --------
+    @log(msg='app: close event')
+    def closeEvent(self, event: QtCore.QEvent):
+
+        # geometry
+        set_setting(key=f'geometry/{self.objectName()}', value=self.geometry())
+
+        #
+        return super().closeEvent(event)
+
+
+class BaseWindow(QtWidgets.QWidget):
 
     def __init__(self, *args, shortcuts: dict[str, str] | None = None, **kwargs):
         super().__init__(*args, **kwargs)
