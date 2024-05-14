5cb669521197d5cb290f644f29a57f26301faf69
Exinker <exinker@gmail.com>
2024-05-14 11:00:38 +0700

    refac:

--- spectrumapp/config.py
+++ spectrumapp/config.py
@@ -1,69 +1,103 @@
 import dataclasses
 import json
 import os
-from abc import ABC, abstractclassmethod, abstractmethod
+from abc import ABC, abstractclassmethod, abstractmethod, abstractproperty
 from dataclasses import dataclass
-from typing import Any, Mapping
+from typing import Mapping
 
-from spectrumapp.exceptions import eprint
 from spectrumapp.types import DirPath, FilePath
+
+
+ENCODING = 'utf-8'
 
 
 class AbstractConfig(ABC):
     """Abstract type for application's config (not GUI)."""
+    FILEPATH = ''
 
-    def __init__(self, version: str, filedir: DirPath, filename: str):
+    def __init__(self, version: str, **data):
         self.version = version  # config's version (have to be corresponded to the application's version)
 
-    def to_json(self, filepath: FilePath) -> None:
-        with open(filepath, 'w', encoding='utf-8') as file:
-            json.dump(self.to_dict(), file)
+        if self.__class__.FILEPATH == '':
+            raise AttributeError('{name}: setup FILEPATH attribute!'.format(
+                name=self.__class__.__name__,
+            ))
 
-    @abstractmethod
-    def to_dict(self) -> Mapping[str, Any]:
-        raise NotImplementedError
+    def dump(self) -> None:
+        """Dump config to file (json)."""
 
-    @classmethod
-    def update(cls, key: str, value: Any, filepath: FilePath) -> None:
+        self._dump(
+            data=self.serialize(),
+        )
+
+    def update(self, attrs: Mapping[str, str | int | float | list]) -> None:
+        """Update config file."""
 
         # load data
-        with open(filepath, 'r', encoding='utf-8') as file:
-            data = json.load(file)
+        data = self._load()
 
         # update data
-        data[key] = value
+        for key, value in attrs.items():
+            data[key] = value
 
         # dump data
-        with open(filepath, 'w', encoding='utf-8') as file:
-            json.dump(data, file)
+        self._dump(
+            data=data,
+        )
+
+    @abstractmethod
+    def serialize(self) -> Mapping[str, str | int | float | list]:
+        """Serialize config."""
+        raise NotImplementedError
 
     # ---------        factory        ---------
     @abstractclassmethod
     def default(cls) -> 'AbstractConfig':
-        raise NotImplementedError
+        """Default config."""
+        data = cls._default()
+
+        return cls(**data)
 
     @classmethod
-    def from_json(cls, filepath: FilePath) -> 'AbstractConfig':
+    def load(cls) -> 'AbstractConfig':
+        """Load config from file (json)."""
 
         # load data
         try:
-            with open(filepath, 'r', encoding='utf-8') as file:
-                data = json.load(file)
+            data = self._load()
 
         except FileNotFoundError as error:
-            eprint(msg=f'{cls.__name__}.from_json: {error}')
-            config = cls.default()
-
-        # validate data
-            pass
+            raise NotImplementedError
 
         # parse data
         try:
             config = cls(**data)
 
         except (json.JSONDecodeError, TypeError, ValueError, KeyError) as error:
-            eprint(msg=f'{cls.__name__}.from_json: {error}')
-            config = cls.default()
+            raise NotImplementedError
 
         #
         return config
+
+    # ---------        private        ---------
+    @abstractclassmethod
+    def _default(cls) -> Mapping[str, str | int | float | list]:
+        """Get default serialized data."""
+        raise NotImplementedError
+
+    @classmethod
+    def _load(self) -> Mapping[str, str | int | float | list]:
+        """Load serialized data."""
+        filepath = self.FILEPATH
+
+        with open(filepath, 'r', encoding=ENCODING) as file:
+            data = json.load(file)
+
+        return data
+
+    def _dump(self, data: Mapping[str, str | int | float | list]) -> None:
+        """Dump serialized data."""
+        filepath = self.FILEPATH
+
+        with open(filepath, 'w', encoding=ENCODING) as file:
+            json.dump(data, file)
