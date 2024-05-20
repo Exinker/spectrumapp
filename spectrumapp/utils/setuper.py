ee6a58481f6d5f43c7c686894bee3dd4ea1f1525
Exinker <exinker@gmail.com>
2024-05-20 10:46:38 +0700

    feat: add prefix

--- spectrumapp/utils/setuper.py
+++ spectrumapp/utils/setuper.py
@@ -3,11 +3,11 @@ from tkinter import Tk
 from PySide6 import QtCore, QtWidgets
 
 
-def getdefault_object_name(obj: QtCore.QObject) -> str:
+def getdefault_object_name(obj: QtCore.QObject, prefix: str = '') -> str:
     """Get a default object name for a given ofject."""
     cls = obj.__class__
 
-    name = cls.__name__
+    name = prefix + cls.__name__
     name = name[0].lower() + name[1:]
 
     return name
