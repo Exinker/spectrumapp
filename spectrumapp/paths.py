aabdf39a58bc490968dfe44303e733c863dc1b76
Exinker <exinker@gmail.com>
2024-07-26 16:49:06 +0700

    feat: remove LIBDIR

--- spectrumapp/paths.py
+++ spectrumapp/paths.py
@@ -5,9 +5,6 @@ from importlib import resources
 import spectrumapp
 
 
-LIBDIR = resources.files(spectrumapp)
-
-
 def pave(relative_path: str) -> str:
     """Pave the absolute path of relative (regardless of running in develop or deploy)."""
 
