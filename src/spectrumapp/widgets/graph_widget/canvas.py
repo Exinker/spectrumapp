037bae4f27dff18d5a87310e8d2448bc04112c79
Exinker <exinker@gmail.com>
2026-05-21 18:30:21 +0700

    feat:

--- src/spectrumapp/widgets/graph_widget/canvas.py
+++ src/spectrumapp/widgets/graph_widget/canvas.py
@@ -1,16 +1,32 @@
+from collections.abc import Mapping
+from typing import Any
+
 import matplotlib as mpl
 from matplotlib.axes._axes import _log as matplotlib_axes_logger
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
 
+matplotlib_axes_logger.setLevel('ERROR')
+
+
 class MplCanvas(FigureCanvas):
 
-    RC_PARAMS = {
+    DEFAULT_RC_PARAMS = {
         'font.family': 'sans-serif',
-        'font.sans-serif': 'Arial',
+        'font.sans-serif': [
+            'Segoe UI',
+            'Microsoft Sans Serif',
+            'MS Sans Serif',
+            'Arial',
+            'DejaVu Sans',
+        ],
         'font.size': 9,
+        'font.weight': '300',
 
+        'figure.facecolor': '#FDFDFD',
+
+        'axes.facecolor': '#FDFDFD',
         'axes.labelsize': 9,
         'axes.linewidth': 1,
         'axes.spines.bottom': True,
@@ -18,21 +34,28 @@ class MplCanvas(FigureCanvas):
         'axes.spines.right': False,
         'axes.spines.top': False,
 
-        'xtick.labelsize': 8,
-        'xtick.major.size': 2.5,
+        'xtick.labelsize': 9,
+        'xtick.major.size': 3,
         'xtick.major.width': 1,
 
-        'ytick.labelsize': 8,
-        'ytick.major.size': 2.5,
+        'ytick.labelsize': 9,
+        'ytick.major.size': 3,
         'ytick.major.width': 1,
     }
     DEFAULT_SIZE = (360, 240)
 
-    def __init__(self, tight_layout: bool = True):
+    def __init__(
+        self,
+        rc_params: Mapping[str, Any] | None = None,
+        tight_layout: bool = True,
+    ):
+        rc_params = rc_params or {}
 
-        matplotlib_axes_logger.setLevel('ERROR')
         mpl.use('Qt5Agg')  # Make sure that we are using QT5
-        mpl.rcParams.update(self.RC_PARAMS)
+
+        params = self.DEFAULT_RC_PARAMS
+        params.update(**rc_params)
+        mpl.rcParams.update(params)
 
         self.fig = Figure()
         self.fig.set_tight_layout(tight_layout)
